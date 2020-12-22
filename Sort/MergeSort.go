b4eec5910a7edace8e9362ad0666db3ff3a96668
KuangjuX <18630816527@163.com>
2020-12-22 09:47:29 +0800

    MergeSort

--- Sort/MergeSort.go
+++ Sort/MergeSort.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func Merge(a []int, l int, mid int, r int){
-	temp := make([]int , r-l+1);
+	temp := make([]int , r-l+5);
 	len := 0
-	i := 1
+	i := l
 	j := mid + 1
 	for i <= mid && j <= r{
 		if a[i] <= a[j]{
@@ -15,7 +15,7 @@ func Merge(a []int, l int, mid int, r int){
 		}else{
 			temp[len] = a[j]
 			len += 1
-			i += 1
+			j += 1
 		}
 	}
 
@@ -30,6 +30,13 @@ func Merge(a []int, l int, mid int, r int){
 		len += 1
 		j += 1
 	}
+
+
+	for m, k := 0, l; k <= r; m, k = m+1, k+1{
+		a[k] = temp[m]
+	}
+
+	
 }
 
 func MergeSort(a []int, l int, r int){
@@ -46,6 +53,7 @@ func MergeSort(a []int, l int, r int){
 
 func main()  {
 	a := []int{3, 1, 2, 5, 6, 43, 4}
+	// a := []int{6, 7, 10, 8, 4, 5, 3}
 	MergeSort(a, 0, len(a)-1)
 
 	fmt.Println(a)
